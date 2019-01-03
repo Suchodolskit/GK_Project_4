e38e0b1167a78dbff85f017db3684dffb7d68884
Tomasz Suchodolski <286407@pw.edu.pl>
2019-01-03 22:53:14 +0100

    ll

--- GK_Project_4/Engine/Camera.cs
+++ GK_Project_4/Engine/Camera.cs
@@ -5,7 +5,8 @@ namespace SoftEngine
     public class Camera
     {
         public Vector3 Position { get; set; }
-        public Vector3 Target { get; set; }
+        public Vector3 Target { get; set;
+        }
 
     }
 }
